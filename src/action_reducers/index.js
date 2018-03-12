f6a3a3dc5144d269d1cda09575c05c581b16596f
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2018-03-12 20:02:06 -0300

    Fix return on action

--- src/action_reducers/index.js
+++ src/action_reducers/index.js
@@ -1,23 +1,22 @@
 export default actionReducers => {
     actionReducers.createAction("itemsHasErrored",          hasErrored => ({hasErrored}));
     actionReducers.createAction("itemsIsLoading",           isLoading  => ({isLoading}));
-    actionReducers.createAction("itemsFetchDataSuccess",    items      => ({items}));
     actionReducers.createAction("itemsFetchData",           url        => {
         return async dispatch => {
             dispatch("itemsIsLoading", true);
 
             let response = await fetch(url)
             if (!response.ok) {
-                dispatch("itemsHasErrored", true);
+                return dispatch("itemsHasErrored", true);
             }
 
             let items = await response.json();
-            dispatch("itemsFetchDataSuccess", items)
 
             dispatch("itemsIsLoading", false);
+            return { items }
         };
     });
-    actionReducers.addCombinedReducer("itemsHasErrored",        "hasErrored", action => action.hasErrored);
-    actionReducers.addCombinedReducer("itemsIsLoading",         "isLoading",  action => action.isLoading,   false);
-    actionReducers.addCombinedReducer("itemsFetchDataSuccess",  "items",      action => action.items,       []);
+    actionReducers.addCombinedReducer("itemsHasErrored", "hasErrored", action => action.hasErrored);
+    actionReducers.addCombinedReducer("itemsIsLoading",  "isLoading",  action => action.isLoading,   false);
+    actionReducers.addCombinedReducer("itemsFetchData",  "items",      action => action.items,       []);
 }
